052c3b43ffe27fedb5a11ae309bbe4dea309dc79
Amjad Elhassan <amjad_elhassan2005@yahoo.com>
2022-09-12 18:00:56 -0400

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,6 +10,8 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Amjad Elhassan </span>
             from <span className="purple"> Khartoum, Sudan.</span><br />
+            currently living in <span className="purple"> Arlington, Va.</span><br />
+
             <br />I studied BioChemistry at the University of North Carolina <br /> (GO TARHEELS) and currently work as a senior front-end leaning full stack developer on the ClassDojo android application.
             <br /> A social platform that connects parent, teachers, and students. It allows teachers to post announcements, accept assignments, award badges for positive and negative behavior, chat with parents of children in a classroom and much more! 
             <br /> The client is written in Typescript + React.js, run on a Next.Js framework, wrapped in a flutter application to handle native android interactions.
