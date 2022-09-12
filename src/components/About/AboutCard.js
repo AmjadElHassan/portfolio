0f556aafb1964ed94dcd8ccd2da55df8ab752eb6
Amjad Elhassan <amjad_elhassan2005@yahoo.com>
2022-09-12 17:59:17 -0400

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,7 +10,9 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Amjad Elhassan </span>
             from <span className="purple"> Khartoum, Sudan.</span><br />
-            <br />I studied BioChemistry at the University of North Carolina <br /> (GO TARHEELS) and currently run Puffin Design Lab, a Tech Consultancy, where I lead a team of Devs and Designers in completing tech-centric projects for local companies and organizations.
+            <br />I studied BioChemistry at the University of North Carolina <br /> (GO TARHEELS) and currently work as a senior front-end leaning full stack developer on the ClassDojo android application.
+            <br /> A social platform that connects parent, teachers, and students. It allows teachers to post announcements, accept assignments, award badges for positive and negative behavior, chat with parents of children in a classroom and much more! 
+            <br /> The client is written in Typescript + React.js, run on a Next.Js framework, wrapped in a flutter application to handle native android interactions.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
